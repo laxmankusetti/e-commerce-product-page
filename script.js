d3d7862b0575199dc4c8958e7f1482a27c8213f4
laxmankusetti <laxmankusetti@gmail.com>
2023-11-04 16:16:55 +0530

    intial implementation

---
+++ script.js
@@ -0,0 +1,81 @@
+const images = [
+    './images/image-product-1.jpg', 
+    './images/image-product-2.jpg', 
+    './images/image-product-3.jpg', 
+    './images/image-product-4.jpg'
+]
+
+let currentImage = 0 
+
+let mobileImage = document.getElementById('mobileImage');
+let forwardButton = document.getElementById('forward');
+let backwardButton = document.getElementById('backward');
+
+forwardButton.addEventListener('click', () => changeImage(1))
+backwardButton.addEventListener('click', () => changeImage(-1))
+
+function changeImage(direction){
+    currentImage += direction
+
+    if(currentImage < 0){
+        currentImage = images.length - 1
+    }
+    else if(currentImage >= images.length - 1){
+        currentImage = 0
+    }
+
+    mobileImage.src = images[currentImage]
+}
+
+let count = document.querySelector('.count');
+let currentCount = 0
+
+function itemsIncrease() {
+    currentCount ++ 
+    count.innerText = currentCount
+}
+
+function itemsDecrease() {
+    if(currentCount){currentCount -- }
+    count.innerText = currentCount
+}
+
+let cartItems = 0
+
+function cartCount(){
+    let cartCount = document.querySelector('.cart-count');
+    cartCount.style.display = 'block';
+    cartItems ++
+    cartCount.innerText = cartItems
+}
+
+const bigImage = document.getElementById('big-image');
+
+function swapImage(newImage, newAlt){
+    bigImage.src = newImage
+    bigImage.alt = newAlt
+}
+
+const smallImgs = document.querySelectorAll('.s-im')
+
+function clickedSmallImg(event){
+    smallImgs.forEach(smallImg => {
+        smallImg.classList.remove('selected')
+    })
+
+    event.target.classList.add('selected')
+}
+
+smallImgs.forEach(smallImg => {
+    smallImg.addEventListener('click', clickedSmallImg)
+})
+
+
+function handleMobMenu(){
+    let mobileMenuContainer = document.querySelector('.mobile-menu-items')
+    mobileMenuContainer.classList.remove('closed')
+}
+
+document.querySelector('.close-menu').onclick = function(){
+    document.querySelector('.mobile-menu-items').classList.add('closed')
+}
